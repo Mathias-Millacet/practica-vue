2ae9135a57b76111db2a25e77bdc00776b06805d
Mathias <100208353+Mathias-Millacet@users.noreply.github.com>
2023-07-22 18:11:45 -0300

    Add files via upload

---
+++ UserList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Lista de usuarios</h1>
+    <UserCard v-for="user in users" :key="user.id" :user="user" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import UserCard from "./UserCard.vue";
+
+export default {
+  components: {
+    UserCard,
+  },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  async mounted() {
+    try {
+      const response = await axios.get("https://dummyjson.com/users");
+      this.users = response.data;
+    } catch (error) {
+      console.error("Error al cargar la lista de usuarios:", error);
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
